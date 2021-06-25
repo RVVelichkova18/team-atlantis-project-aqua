fc26e094aacb3cdcec6ce2b0de8a5fc70983388c
Kalin Chervenkov <KSChervenkov19@codingburgas.bg>
2021-06-26 01:00:53 +0300

    Finish the info page

--- Website/JavaScript/charts.js
+++ Website/JavaScript/charts.js
@@ -79,7 +79,7 @@ am4core.ready(function() {
 
 });
 
-//-------------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------------------
 
 am4core.ready(function() {
 
@@ -153,4 +153,57 @@ am4core.ready(function() {
     // Cursor
     chart.cursor = new am4charts.XYCursor();
     
-    }); // end am4core.ready()
+});
+
+//-------------------------------------------------------------------------------------------------------------------
+
+am4core.ready(function() {
+
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+    
+    // Create chart instance
+    var chart = am4core.create("chartdiv2", am4charts.PieChart);
+    
+    // Add data
+    chart.data = [ {
+        "country": "Пода",
+        "litres": 307
+    }, {
+        "country": "Атанасовско езеро",
+        "litres": 1404
+    }, {
+        "country": "Поморийско езеро",
+        "litres": 814
+    }, {
+        "country": "Езеро Узунген",
+        "litres": 211
+    }, {
+        "country": "Бургаско езеро",
+        "litres": 2899
+    }, {
+        "country": "Язовир мандра",
+        "litres": 1300
+    }, {
+        "country": "Солници",
+        "litres": 965
+    }];
+    
+    // Set inner radius
+    chart.innerRadius = am4core.percent(50);
+    
+    // Add and configure Series
+    var pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = "litres";
+    pieSeries.dataFields.category = "country";
+    pieSeries.slices.template.stroke = am4core.color("#fff");
+    pieSeries.slices.template.strokeWidth = 2;
+    pieSeries.slices.template.strokeOpacity = 1;
+    
+    // This creates initial animation
+    pieSeries.hiddenState.properties.opacity = 1;
+    pieSeries.hiddenState.properties.endAngle = -90;
+    pieSeries.hiddenState.properties.startAngle = -90;
+    
+    });
