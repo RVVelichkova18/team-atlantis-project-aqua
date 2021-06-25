96ba5c2da6be8db8369c9424449eec6e6f914c98
Kalin Chervenkov <KSChervenkov19@codingburgas.bg>
2021-06-25 23:55:56 +0300

    Add information chart

--- Website/JavaScript/charts.js
+++ Website/JavaScript/charts.js
@@ -77,4 +77,80 @@ am4core.ready(function() {
     capacityLabel.textAlign = "middle";
     capacityLabel.padding(0, 0, 0, 0);
 
-}); // end am4core.ready()
+});
+
+//-------------------------------------------------------------------------------------------------------------------//
+
+am4core.ready(function() {
+
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+    
+    // Create chart instance
+    var chart = am4core.create("chartdiv1", am4charts.XYChart);
+    chart.scrollbarX = new am4core.Scrollbar();
+    
+    // Add data
+    chart.data = [{
+        "country": "2014",
+        "visits": 5846
+    }, {
+        "country": "2015",
+        "visits": 5845
+    }, {
+        "country": "2016",
+        "visits": 5845
+    }, {
+        "country": "2017",
+        "visits": 5847
+    }, {
+        "country": "2018",
+        "visits": 5849
+    }, {
+        "country": "2019",
+        "visits": 5849
+    }];
+    
+    // Create axes
+    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "country";
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.minGridDistance = 30;
+    categoryAxis.renderer.labels.template.horizontalCenter = "right";
+    categoryAxis.renderer.labels.template.verticalCenter = "middle";
+    categoryAxis.renderer.labels.template.rotation = 270;
+    categoryAxis.tooltip.disabled = true;
+    categoryAxis.renderer.minHeight = 110;
+    
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.renderer.minWidth = 50;
+    
+    // Create series
+    var series = chart.series.push(new am4charts.ColumnSeries());
+    series.sequencedInterpolation = true;
+    series.dataFields.valueY = "visits";
+    series.dataFields.categoryX = "country";
+    series.tooltipText = "[{categoryX}: bold]{valueY}[/]";
+    series.columns.template.strokeWidth = 0;
+    
+    series.tooltip.pointerOrientation = "vertical";
+    
+    series.columns.template.column.cornerRadiusTopLeft = 10;
+    series.columns.template.column.cornerRadiusTopRight = 10;
+    series.columns.template.column.fillOpacity = 0.8;
+    
+    // on hover, make corner radiuses bigger
+    var hoverState = series.columns.template.column.states.create("hover");
+    hoverState.properties.cornerRadiusTopLeft = 0;
+    hoverState.properties.cornerRadiusTopRight = 0;
+    hoverState.properties.fillOpacity = 1;
+    
+    series.columns.template.adapter.add("fill", function(fill, target) {
+        return chart.colors.getIndex(target.dataItem.index);
+    });
+    
+    // Cursor
+    chart.cursor = new am4charts.XYCursor();
+    
+    }); // end am4core.ready()
